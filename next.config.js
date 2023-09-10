202134084bd8cf9ec5c3c990c44c2997a5bd326b
Kirk Daniels <kirkd@ayoba.me>
2023-09-10 14:03:31 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const contentful = require("contentful");
 
 module.exports = {
-  images: {
-    loader: "imgix",
-    path: "https://epokothweni.imgix.net",
-    domains: ["images.ctfassets.net"],
-  },
+  // images: {
+  //   loader: "imgix",
+  //   path: "https://kdee.imgix.net",
+  //   domains: ["images.ctfassets.net"],
+  // },
 };
